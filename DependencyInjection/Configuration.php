6980fd547c1b31a8f8bcf090a82baca5a8c8750e
Eric Abouaf <eric.abouaf@gmail.com>
2020-06-15 15:07:37 +0200

    Handling Symfony 4.3 getRootNode deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,8 +15,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('lexik_currency');
+        $treeBuilder = new TreeBuilder('lexik_currency');
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('lexik_currency');
 
         $rootNode
             ->addDefaultsIfNotSet()
